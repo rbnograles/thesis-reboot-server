d18c8c9edf331ca82172eb5ef39264fa94bb85f0
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-10-20 20:01:13 +0800

    Update: start options and incorporate swagger api ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('JuanBreath Api Documentation')
+    .setDescription('This is the interactive JB Api documentation')
+    .setVersion('2.0.0')
+    .addTag('JuanBreath Contact Tracing AppModule')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
