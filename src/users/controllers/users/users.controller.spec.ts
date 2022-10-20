e18e5489d37787607930a6802261b15797bbc6fb
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-10-20 17:27:16 +0800

    Add: User controller and test spec

---
+++ src/users/controllers/users/users.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('should return hello world', () => {
+    expect(controller.getUsers()).toEqual({ msg: `Hello World!` });
+  });
+});
