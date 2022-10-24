7782a3d4e78b21152e7cf3e6985784eab19d4ea3
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-10-24 08:22:05 +0800

    Initialize env file, and mongodb connectors as well as config file for parsing env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, AuthenticationModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.dev.env' }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    UsersModule,
+    AuthenticationModule,
+  ],
   controllers: [],
   providers: [],
 })
