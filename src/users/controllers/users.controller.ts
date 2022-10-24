fbb2d650c8092f9a126cf9be0b04faf83e4aa13d
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-10-24 14:14:26 +0800

    Rearrange function order to align with CRUD

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -11,12 +11,25 @@ import {
 import { UserService } from '../services/user.service';
 import { User } from '../schemas/user.schema';
 import { CreateUserDto } from '../dto/CreateUser.dto';
-import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
   // inject user service to user controller
   constructor(private userService: UserService) {}
+
+  @Post('/create')
+  async createUser(@Body() userData: CreateUserDto) {
+    // process the supplied data to the service
+    const createdUserData = await this.userService.createOneUserAccount(
+      userData,
+    );
+
+    return {
+      success: true,
+      message: 'User is created successfully!',
+      body: createdUserData,
+    };
+  }
 
   @Get()
   async getUsers(): Promise<User[]> {
@@ -32,19 +45,5 @@ export class UsersController {
     if (!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
 
     return user;
-  }
-
-  @Post('/create')
-  async createUser(@Body() userData: CreateUserDto, @Res() res: Response) {
-    // process the supplied data to the service
-    const createdUserData = await this.userService.createOneUserAccount(
-      userData,
-    );
-
-    return res.send({
-      success: true,
-      message: 'User is created successfully!',
-      body: createdUserData,
-    });
   }
 }
