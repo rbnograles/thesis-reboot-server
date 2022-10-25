e2f42b059eac3cf843705df8a9706223d754cb8e
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-10-25 17:12:45 +0800

    added abstract entity repository for CRUD functions

---
+++ src/db/entity.repository.ts
@@ -0,0 +1,45 @@
+import { Document, FilterQuery, Model, UpdateQuery } from 'mongoose';
+
+export abstract class EntityRepository<T extends Document> {
+  constructor(protected readonly entityModel: Model<T>) {}
+
+  async findOne(
+    entityFilterQuery: FilterQuery<T>,
+    projection?: Record<string, unknown>,
+  ): Promise<T | null> {
+    return this.entityModel
+      .findOne(entityFilterQuery, {
+        _id: 0,
+        __v: 0,
+        ...projection,
+      })
+      .exec();
+  }
+
+  async find(entityFilterQuery: FilterQuery<T>): Promise<T[] | null> {
+    return this.entityModel.find(entityFilterQuery);
+  }
+
+  async create(entity: unknown): Promise<T> {
+    const newEntity = new this.entityModel(entity);
+    return newEntity.save();
+  }
+
+  async findOneAndUpdate(
+    entityFilterQuery: FilterQuery<T>,
+    updateEntityData: UpdateQuery<unknown>,
+  ): Promise<T | null> {
+    return this.entityModel.findOneAndUpdate(
+      entityFilterQuery,
+      updateEntityData,
+      {
+        new: true,
+      },
+    );
+  }
+
+  async deleteMany(entityFilterQuery: FilterQuery<T>): Promise<boolean> {
+    const deleteResult = await this.entityModel.deleteMany(entityFilterQuery);
+    return deleteResult.deletedCount >= 1;
+  }
+}
