df3161e1e229d249f348fdad4fd11999469655e4
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-10-27 07:27:03 +0800

    remove current auth to setup proper auth in nest

--- src/authentication/authentication.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthenticationController } from './controllers/authentication.controller';
-
-@Module({
-  controllers: [AuthenticationController],
-})
-export class AuthenticationModule {}
