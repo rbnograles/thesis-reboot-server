dbe97b7e4b5ea6774ca3044143a60970dcdb6a17
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-10-27 13:11:29 +0800

    Update validation schema

--- src/users/dto/UpdateUser.dto.ts
+++ src/users/dto/UpdateUser.dto.ts
@@ -1,18 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsBoolean } from 'class-validator';
 
 export class UpdateUserDto {
-  @ApiProperty()
+  @ApiProperty({
+    required: true,
+  })
+  @IsNotEmpty({ message: 'Mobile number should not be empty' })
+  @IsString({ message: 'Mobile number should be a string' })
   mobileNumber: string;
 
-  @ApiProperty({
-    default: 'Normal',
-    required: false,
-  })
+  @ApiProperty()
+  @IsNotEmpty({ message: 'User health status should not be empty' })
+  @IsString({ message: 'User health status should be a string' })
   userHealthStatus: string;
 
   @ApiProperty()
+  @IsNotEmpty({ message: 'Is verified should not be empty' })
+  @IsBoolean({ message: 'Is verified must be a boolean' })
   isVerified: boolean;
 
   @ApiProperty()
+  @IsNotEmpty({ message: 'User type should not be empty' })
+  @IsString({ message: 'User type should be a string' })
   userType: string;
 }
