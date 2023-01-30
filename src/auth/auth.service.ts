6e3917b140580ab1be46a5a390635f01ecd1e226
rbnograles <ryannograles.025.gtsi@gmail.com>
2023-01-30 15:11:01 +0800

    Added user service spec file

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  //   async validateUser(mobileNumber: string, password: string): Promise<any> {
+  //     const user = await this.userService.fetchOneByMobileNumber(mobileNumber);
+  //   }
+}
