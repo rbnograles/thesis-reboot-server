4c3b1a4d33375ccf390ffd52a28d8d2a8bbd892a
rbnograles <ryannograles.025.gtsi@gmail.com>
2023-01-30 17:57:34 +0800

    added test case for user service

--- src/users/__mocks__/users.service.ts
+++ src/users/__mocks__/users.service.ts
@@ -4,5 +4,7 @@ export const UsersService = jest.fn().mockReturnValue({
   createOneUserAccount: jest.fn().mockResolvedValue(userStub()),
   fetchUsers: jest.fn().mockResolvedValue([userStub()]),
   fetchOneUser: jest.fn().mockResolvedValue(userStub()),
+  fetchOneByMobileNumber: jest.fn().mockResolvedValue(userStub()),
+  checkIfUserIsExisting: jest.fn().mockResolvedValue(true),
   findOneAndUpdate: jest.fn().mockResolvedValue(userStub()),
 });
--- src/users/test/users.service.spec.ts
+++ src/users/test/users.service.spec.ts
@@ -1,7 +1,5 @@
-import { HttpException } from '@nestjs/common';
 import mongoose from 'mongoose';
 import { CreateUserDto } from '../dto/CreateUser.dto';
-import { User } from '../schemas/user.schema';
 import { UsersService } from '../users.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { userStub } from './stubs/user.stub';
@@ -12,6 +10,8 @@ jest.mock('../users.service.ts');
 describe('UserService', () => {
   let service: UsersService;
   let filterQuery: string;
+  let createUserDTO: CreateUserDto;
+  let updateUserDTO: UpdateUserDto;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,6 +23,24 @@ describe('UserService', () => {
     service = module.get<UsersService>(UsersService);
     filterQuery = new mongoose.Types.ObjectId().toString();
     // clear mocks after every execution
+
+    updateUserDTO = {
+      mobileNumber: userStub().mobileNumber,
+      userHealthStatus: userStub().userHealthStatus,
+      userType: userStub().userType,
+      isVerified: userStub().isVerified,
+      password: userStub().password,
+    };
+
+    createUserDTO = {
+      mobileNumber: userStub().mobileNumber,
+      userHealthStatus: userStub().userHealthStatus,
+      userType: userStub().userType,
+      isVerified: userStub().isVerified,
+      createdAt: userStub().createdAt,
+      password: userStub().password,
+    };
+
     jest.clearAllMocks();
   });
 
@@ -30,11 +48,75 @@ describe('UserService', () => {
    * Default Testing if the controller is defined
    */
   describe('variables must be defined', () => {
-    it('service should be defined', () => {
+    it('should be defined', () => {
       expect(service).toBeDefined();
     });
     it('filterQuery should be defined', () => {
       expect(filterQuery).toBeDefined();
+    });
+  });
+
+  describe('when createOneUserAccount is called', () => {
+    test('should call createOneUserAccount method with expected params', async () => {
+      const createOneUserAccountSpy = jest.spyOn(
+        service,
+        'createOneUserAccount',
+      );
+      const dto = createUserDTO;
+      service.createOneUserAccount(dto);
+      expect(createOneUserAccountSpy).toHaveBeenCalledWith(dto);
+    });
+  });
+
+  describe('when fetchUsers is called', () => {
+    test('should call fetchUsers method with no parameter', async () => {
+      const fetchUsersSpy = jest.spyOn(service, 'fetchUsers');
+      service.fetchUsers();
+      expect(fetchUsersSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('when fetchOneUser is called', () => {
+    test('should call fetchOneUser method with _id as parameter', async () => {
+      const fetchOneUsersSpy = jest.spyOn(service, 'fetchOneUser');
+      service.fetchOneUser(filterQuery);
+      expect(fetchOneUsersSpy).toHaveBeenCalledWith(filterQuery);
+    });
+  });
+
+  describe('when fetchOneByMobileNumber is called', () => {
+    test('should call fetchOneByMobileNumber method with mobileNumber as parameter', async () => {
+      const fetchUsersByMobileNumberSpy = jest.spyOn(
+        service,
+        'fetchOneByMobileNumber',
+      );
+      const mobileNumber = userStub().mobileNumber;
+      service.fetchOneByMobileNumber(mobileNumber);
+      expect(fetchUsersByMobileNumberSpy).toHaveBeenCalledWith(mobileNumber);
+    });
+  });
+
+  describe('when checkIfUserIsExisting is called', () => {
+    test('should call checkIfUserIsExisting method with key and args as parameter', async () => {
+      const checkIfUserIsExistingSpy = jest.spyOn(
+        service,
+        'checkIfUserIsExisting',
+      );
+      const mobileNumber = userStub().mobileNumber;
+      service.checkIfUserIsExisting('mobileNumber', mobileNumber);
+      expect(checkIfUserIsExistingSpy).toHaveBeenCalledWith(
+        'mobileNumber',
+        mobileNumber,
+      );
+    });
+  });
+
+  describe('when findOneAndUpdate is called', () => {
+    test('should call findOneAndUpdate method with expected params', async () => {
+      const findOneAndUpdateSpy = jest.spyOn(service, 'findOneAndUpdate');
+      const dto = updateUserDTO;
+      service.findOneAndUpdate(filterQuery, dto);
+      expect(findOneAndUpdateSpy).toHaveBeenCalledWith(filterQuery, dto);
     });
   });
 });
